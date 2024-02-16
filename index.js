17822f2f48c6d808f19c5af3a601be764fd1ea12
Dominic <dominicepaul@gmail.com>
2024-02-16 12:21:51 +0000

    starting coding the index.js file. Started with the questions and user to input

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { type } = require("os");
+
+// const writeFileAsync = until.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = [
-
-];
+const questions = () => 
+    inquirer.questions([
+    {
+        type: "input",
+        name: "title",
+        message: "<Project-Title-Here>",
+    },
+    { 
+        type: "input",
+        name: "description",
+        message: "Please describe your your project is. A short paragraph explaining the use case and how you came about creating it.",
+    },
+    {
+        type: ""
+    }
+    ])
+;
 
 // function to write README file
 function writeToFile(fileName, data) {
